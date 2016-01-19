07b18005293a5e8bdd009aa417e82009c885fe9d
uberspot <onexemailx@gmail.com>
2016-01-19 13:32:32 +0100

    add screenlayout scripts for autostart

---
+++ .screenlayout/hdmi1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --off --output VGA1 --off
